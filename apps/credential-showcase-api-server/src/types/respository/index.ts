5d08270d8b43ccb07bd5cd56c932644503c28e4c
A.G.J. Cate <brummos@gmail.com>
2025-02-07 10:34:01 +0100

    chore: interface update

--- apps/credential-showcase-api-server/src/types/respository/index.ts
+++ apps/credential-showcase-api-server/src/types/respository/index.ts
@@ -1,9 +1,9 @@
 import { Asset, NewAsset } from '../schema';
 
 export type AssetRepositoryDefinition = {
-    findById(assetId: string): Promise<Asset>;
+    findById(id: string): Promise<Asset>;
     findAll(): Promise<Asset[]>;
     create(asset: NewAsset): Promise<Asset>;
     update(id: string, asset: Asset): Promise<Asset>;
-    delete(assetId: string): Promise<void>;
+    delete(id: string): Promise<void>;
 }

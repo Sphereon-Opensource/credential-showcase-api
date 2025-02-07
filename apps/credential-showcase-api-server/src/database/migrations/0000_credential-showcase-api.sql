979d7d28bc855b8a0103577a5462c3a4b756c80d
A.G.J. Cate <brummos@gmail.com>
2025-02-07 11:09:22 +0100

    chore: use snake case in db

--- apps/credential-showcase-api-server/src/database/migrations/0000_credential-showcase-api.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "asset" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"mediaType" varchar(255) NOT NULL,
-	"fileName" varchar(255),
-	"description" varchar(255),
-	"content" "bytea" NOT NULL
-);

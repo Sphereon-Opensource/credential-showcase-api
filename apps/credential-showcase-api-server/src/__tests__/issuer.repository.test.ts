23fbfe500226608f9807ecba54115518a888542c
A.G.J. Cate <brummos@gmail.com>
2025-02-20 08:32:55 +0100

    feat: implemented issuer endpoints

--- apps/credential-showcase-api-server/src/__tests__/issuer.repository.test.ts
+++ apps/credential-showcase-api-server/src/__tests__/issuer.repository.test.ts
@@ -1,36 +1,98 @@
 import 'reflect-metadata';
-import { PgDatabase } from 'drizzle-orm/pg-core';
-import { drizzle } from 'drizzle-orm/pglite'
+import { PGlite } from '@electric-sql/pglite';
+import { drizzle } from 'drizzle-orm/pglite';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Container } from 'typedi';
-import { DatabaseService } from '../services/DatabaseService';
-import { NewIssuer } from '../types'
-import IssuerRepository from '../database/repositories/IssuerRepository'
+import DatabaseService from '../services/DatabaseService';
+import IssuerRepository from '../database/repositories/IssuerRepository';
+import AssetRepository from '../database/repositories/AssetRepository';
+import CredentialDefinitionRepository from '../database/repositories/CredentialDefinitionRepository';
+import * as schema from '../database/schema';
+import {
+  Asset,
+  CredentialAttributeType,
+  CredentialDefinition,
+  CredentialType,
+  NewAsset,
+  NewCredentialDefinition,
+  NewIssuer,
+  IssuerType
+} from '../types';
 
 describe('Database issuer repository tests', (): void => {
+  let client: PGlite;
   let repository: IssuerRepository;
+  let credentialDefinition1: CredentialDefinition;
+  let credentialDefinition2: CredentialDefinition;
+  let asset: Asset;
 
   beforeEach(async (): Promise<void> => {
-    const database: PgDatabase<any> = drizzle();
+    client = new PGlite();
+    const database = drizzle(client, { schema }) as unknown as NodePgDatabase;
     await migrate(database, { migrationsFolder: './apps/credential-showcase-api-server/src/database/migrations' })
     const mockDatabaseService = {
       getConnection: jest.fn().mockResolvedValue(database),
     };
     Container.set(DatabaseService, mockDatabaseService);
     repository = Container.get(IssuerRepository);
+    const assetRepository = Container.get(AssetRepository);
+    const newAsset: NewAsset = {
+      mediaType: 'image/png',
+      fileName: 'image.png',
+      description: 'some image',
+      content: Buffer.from('some binary data'),
+    };
+    asset = await assetRepository.create(newAsset)
+    const credentialDefinitionRepository = Container.get(CredentialDefinitionRepository);
+    const newCredentialDefinition: NewCredentialDefinition = {
+      name: 'example_name',
+      version: 'example_version',
+      icon: asset.id,
+      type: CredentialType.ANONCRED,
+      attributes: [
+        {
+          name: 'example_attribute_name1',
+          value: 'example_attribute_value1',
+          type: CredentialAttributeType.STRING
+        },
+        {
+          name: 'example_attribute_name2',
+          value: 'example_attribute_value2',
+          type: CredentialAttributeType.STRING
+        }
+      ],
+      representations: [
+        { // TODO SHOWCASE-81 OCARepresentation
+
+        },
+        { // TODO SHOWCASE-81 OCARepresentation
+
+        }
+      ],
+      revocation: { // TODO SHOWCASE-80 AnonCredRevocation
+        title: 'example_revocation_title',
+        description: 'example_revocation_description'
+      }
+    };
+    credentialDefinition1 = await credentialDefinitionRepository.create(newCredentialDefinition)
+    credentialDefinition2 = await credentialDefinitionRepository.create(newCredentialDefinition)
   })
 
-  afterEach((): void => {
+  afterEach(async (): Promise<void> => {
+    await client.close();
     jest.resetAllMocks();
     Container.reset();
   });
 
   it('Should save issuer to database', async (): Promise<void> => {
     const issuer: NewIssuer = {
-      name: 'test_issuer',
-      type: 'ARIES',
-      description: 'test_issuer',
-      organization: 'test_organization'
+      name: 'example_name',
+      type: IssuerType.ARIES,
+      credentialDefinitions: [credentialDefinition1.id, credentialDefinition2.id],
+      description: 'example_description',
+      organization: 'example_organization',
+      logo: asset.id,
     };
 
     const savedIssuer = await repository.create(issuer)
@@ -40,14 +102,37 @@ describe('Database issuer repository tests', (): void => {
     expect(savedIssuer.type).toEqual(issuer.type)
     expect(savedIssuer.description).toEqual(issuer.description)
     expect(savedIssuer.organization).toEqual(issuer.organization);
+    expect(savedIssuer.credentialDefinitions.length).toEqual(2);
+    expect(savedIssuer.logo).not.toBeNull()
+    expect(savedIssuer.logo!.id).toBeDefined()
+    expect(savedIssuer.logo!.mediaType).toEqual(asset.mediaType)
+    expect(savedIssuer.logo!.fileName).toEqual(asset.fileName)
+    expect(savedIssuer.logo!.description).toEqual(asset.description)
+    expect(savedIssuer.logo!.content).toStrictEqual(asset.content)
+  })
+
+  it('Should throw error when saving issuer with invalid logo id', async (): Promise<void> => {
+    const unknownIconId = 'a197e5b2-e4e5-4788-83b1-ecaa0e99ed3a';
+    const issuer: NewIssuer = {
+      name: 'example_name',
+      type: IssuerType.ARIES,
+      credentialDefinitions: [credentialDefinition1.id, credentialDefinition2.id],
+      description: 'example_description',
+      organization: 'example_organization',
+      logo: unknownIconId,
+    };
+
+    await expect(repository.create(issuer)).rejects.toThrowError(`No asset found for id: ${unknownIconId}`)
   })
 
   it('Should get issuer by id from database', async (): Promise<void> => {
     const issuer: NewIssuer = {
-      name: 'test_issuer',
-      type: 'ARIES',
-      description: 'test_issuer',
-      organization: 'test_organization'
+      name: 'example_name',
+      type: IssuerType.ARIES,
+      credentialDefinitions: [credentialDefinition1.id, credentialDefinition2.id],
+      description: 'example_description',
+      organization: 'example_organization',
+      logo: asset.id,
     };
 
     const savedIssuer = await repository.create(issuer)
@@ -55,32 +140,33 @@ describe('Database issuer repository tests', (): void => {
 
     const fromDb = await repository.findById(savedIssuer.id)
 
-    expect(fromDb).not.toBeNull()
-    expect(savedIssuer.name).toEqual(issuer.name)
-    expect(savedIssuer.type).toEqual(issuer.type)
-    expect(savedIssuer.description).toEqual(issuer.description)
-    expect(savedIssuer.organization).toEqual(issuer.organization);
+    expect(fromDb).toBeDefined()
+    expect(fromDb.name).toEqual(issuer.name)
+    expect(fromDb.type).toEqual(issuer.type)
+    expect(fromDb.description).toEqual(issuer.description)
+    expect(fromDb.organization).toEqual(issuer.organization);
+    expect(fromDb.credentialDefinitions.length).toEqual(2);
+    expect(fromDb.logo).not.toBeNull()
+    expect(fromDb.logo!.id).toBeDefined()
+    expect(fromDb.logo!.mediaType).toEqual(asset.mediaType)
+    expect(fromDb.logo!.fileName).toEqual(asset.fileName)
+    expect(fromDb.logo!.description).toEqual(asset.description)
+    expect(fromDb.logo!.content).toStrictEqual(asset.content)
   })
 
   it('Should get all issuers from database', async (): Promise<void> => {
-    const issuer1: NewIssuer = {
-      name: 'test_issuer1',
-      type: 'ARIES',
-      description: 'test_issuer1',
-      organization: 'test_organization1'
+    const issuer: NewIssuer = {
+      name: 'example_name',
+      type: IssuerType.ARIES,
+      credentialDefinitions: [credentialDefinition1.id],
+      description: 'example_description',
+      organization: 'example_organization'
     };
 
-    const savedIssuer1 = await repository.create(issuer1)
+    const savedIssuer1 = await repository.create(issuer)
     expect(savedIssuer1).toBeDefined()
 
-    const issuer2: NewIssuer = {
-      name: 'test_issuer2',
-      type: 'ARIES',
-      description: 'test_issuer2',
-      organization: 'test_organization2'
-    };
-
-    const savedIssuer2 = await repository.create(issuer2)
+    const savedIssuer2 = await repository.create(issuer)
     expect(savedIssuer2).toBeDefined()
 
     const fromDb = await repository.findAll()
@@ -90,10 +176,11 @@ describe('Database issuer repository tests', (): void => {
 
   it('Should delete issuer from database', async (): Promise<void> => {
     const issuer: NewIssuer = {
-      name: 'test_issuer',
-      type: 'ARIES',
-      description: 'test_issuer',
-      organization: 'test_organization'
+      name: 'example_name',
+      type: IssuerType.ARIES,
+      credentialDefinitions: [credentialDefinition1.id],
+      description: 'example_description',
+      organization: 'example_organization'
     };
 
     const savedIssuer = await repository.create(issuer)
@@ -106,22 +193,59 @@ describe('Database issuer repository tests', (): void => {
 
   it('Should update issuer in database', async (): Promise<void> => {
     const issuer: NewIssuer = {
-      name: 'test_issuer',
-      type: 'ARIES',
-      description: 'test_issuer',
-      organization: 'test_organization'
+      name: 'example_name',
+      type: IssuerType.ARIES,
+      credentialDefinitions: [credentialDefinition1.id, credentialDefinition2.id],
+      description: 'example_description',
+      organization: 'example_organization',
+      logo: asset.id,
     };
 
     const savedIssuer = await repository.create(issuer)
     expect(savedIssuer).toBeDefined()
 
-    const newName = 'new_test_issuer'
-    const updatedIssuer = await repository.update(savedIssuer.id, { ...savedIssuer, name: newName })
+    const newName = 'new_name'
+    const updatedIssuer = await repository.update(savedIssuer.id, {
+      ...savedIssuer,
+      name: newName,
+      credentialDefinitions: [credentialDefinition1.id],
+      logo: savedIssuer.logo?.id,
+    })
 
     expect(updatedIssuer).toBeDefined()
-    expect(savedIssuer.name).toEqual(issuer.name)
-    expect(savedIssuer.type).toEqual(issuer.type)
-    expect(savedIssuer.description).toEqual(issuer.description)
-    expect(savedIssuer.organization).toEqual(issuer.organization);
+    expect(updatedIssuer.name).toEqual(newName)
+    expect(updatedIssuer.type).toEqual(issuer.type)
+    expect(updatedIssuer.description).toEqual(issuer.description)
+    expect(updatedIssuer.organization).toEqual(issuer.organization);
+    expect(updatedIssuer.credentialDefinitions.length).toEqual(1);
+    expect(updatedIssuer.logo).not.toBeNull()
+    expect(updatedIssuer.logo!.id).toBeDefined()
+    expect(updatedIssuer.logo!.mediaType).toEqual(asset.mediaType)
+    expect(updatedIssuer.logo!.fileName).toEqual(asset.fileName)
+    expect(updatedIssuer.logo!.description).toEqual(asset.description)
+    expect(updatedIssuer.logo!.content).toStrictEqual(asset.content)
+  })
+
+  it('Should throw error when updating issuer with invalid logo id', async (): Promise<void> => {
+    const unknownIconId = 'a197e5b2-e4e5-4788-83b1-ecaa0e99ed3a';
+    const issuer: NewIssuer = {
+      name: 'example_name',
+      type: IssuerType.ARIES,
+      credentialDefinitions: [credentialDefinition1.id, credentialDefinition2.id],
+      description: 'example_description',
+      organization: 'example_organization',
+      logo: asset.id,
+    };
+
+    const savedIssuer = await repository.create(issuer)
+    expect(savedIssuer).toBeDefined()
+
+    const updatedIssuer: NewIssuer = {
+      ...savedIssuer,
+      credentialDefinitions: [credentialDefinition1.id],
+      logo: unknownIconId,
+    }
+
+    await expect(repository.update(savedIssuer.id, updatedIssuer)).rejects.toThrowError(`No asset found for id: ${unknownIconId}`)
   })
 })
--- apps/credential-showcase-api-server/src/database/repositories/IssuerRepository.ts
+++ apps/credential-showcase-api-server/src/database/repositories/IssuerRepository.ts
@@ -1,21 +1,55 @@
-import { eq } from 'drizzle-orm';
+import { eq, inArray } from 'drizzle-orm';
 import { Service } from 'typedi';
-import { DatabaseService } from '../../services/DatabaseService';
+import DatabaseService from '../../services/DatabaseService';
+import AssetRepository from './AssetRepository';
 import { NotFoundError } from '../../errors';
-import { issuers } from '../schema';
+import { credentialDefinitions, issuers, issuersToCredentialDefinitions } from '../schema';
 import { Issuer, NewIssuer, RepositoryDefinition } from '../../types';
 
 @Service()
 class IssuerRepository implements RepositoryDefinition<Issuer, NewIssuer> {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(
+      private readonly databaseService: DatabaseService,
+      private readonly assetRepository: AssetRepository
+  ) {}
 
   async create(issuer: NewIssuer): Promise<Issuer> {
-    const [result] = await (await this.databaseService.getConnection())
-      .insert(issuers)
-      .values(issuer)
-      .returning();
+    const logoResult = issuer.logo ? await this.assetRepository.findById(issuer.logo) : null
 
-    return result
+    return (await this.databaseService.getConnection()).transaction(async (tx): Promise<Issuer> => {
+      const [issuerResult] = (await tx.insert(issuers)
+          .values({
+            name: issuer.name,
+            type: issuer.type,
+            description: issuer.description,
+            organization: issuer.organization,
+            logo: logoResult ? logoResult.id : null
+          })
+          .returning())
+
+      const issuersToCredentialDefinitionsResult = await tx.insert(issuersToCredentialDefinitions)
+          .values(issuer.credentialDefinitions.map((credentialDefinitionId: string) => ({
+            issuer: issuerResult.id,
+            credentialDefinition: credentialDefinitionId
+          })))
+          .returning();
+
+      const credentialDefinitionsResult = await tx.query.credentialDefinitions.findMany({
+        where: inArray(credentialDefinitions.id, issuersToCredentialDefinitionsResult.map(item => item.credentialDefinition)),
+        with: {
+          attributes: true,
+          representations: true,
+          revocation: true,
+          icon: true
+        },
+      })
+
+      return {
+        ...issuerResult,
+        logo: logoResult,
+        credentialDefinitions: credentialDefinitionsResult
+      };
+    })
   }
 
   async delete(id: string): Promise<void> {
@@ -27,31 +61,100 @@ class IssuerRepository implements RepositoryDefinition<Issuer, NewIssuer> {
 
   async update(id: string, issuer: NewIssuer): Promise<Issuer> {
     await this.findById(id)
-    const [result] = await (await this.databaseService.getConnection())
-      .update(issuers)
-      .set(issuer)
-      .returning();
 
-    return result
+    const logoResult = issuer.logo ? await this.assetRepository.findById(issuer.logo) : null
+    return (await this.databaseService.getConnection()).transaction(async (tx): Promise<Issuer> => {
+      const [issuerResult] = await tx.update(issuers)
+          .set({
+            name: issuer.name,
+            type: issuer.type,
+            description: issuer.description,
+            organization: issuer.organization,
+            logo: logoResult ? logoResult.id : null
+          })
+          .where(eq(issuers.id, id))
+          .returning();
+
+      await tx.delete(issuersToCredentialDefinitions).where(eq(issuersToCredentialDefinitions.issuer, id))
+
+      const issuersToCredentialDefinitionsResult = await tx.insert(issuersToCredentialDefinitions)
+          .values(issuer.credentialDefinitions.map((credentialDefinitionId: string) => ({
+            issuer: issuerResult.id,
+            credentialDefinition: credentialDefinitionId
+          })))
+          .returning();
+
+      const credentialDefinitionsResult = await tx.query.credentialDefinitions.findMany({
+        where: inArray(credentialDefinitions.id, issuersToCredentialDefinitionsResult.map(item => item.credentialDefinition)),
+        with: {
+          attributes: true,
+          representations: true,
+          revocation: true,
+          icon: true
+        },
+      })
+
+      return {
+        ...issuerResult,
+        logo: logoResult,
+        credentialDefinitions: credentialDefinitionsResult
+      };
+    })
   }
 
   async findById(id: string): Promise<Issuer> {
-    const [result] = await (await this.databaseService.getConnection())
-      .select()
-      .from(issuers)
-      .where(eq(issuers.id, id));
+    const result = await (await this.databaseService.getConnection()).query.issuers.findFirst({
+      where: eq(issuers.id, id),
+      with: {
+        credentialDefinitions: {
+          with: {
+            credentialDefinition: {
+              with: {
+                icon: true,
+                attributes: true,
+                representations: true,
+                revocation: true
+              }
+            }
+          }
+        },
+        logo: true
+      },
+    })
 
     if (!result) {
       return Promise.reject(new NotFoundError(`No issuer found for id: ${id}`))
     }
 
-    return result
+    return {
+      ...result,
+      credentialDefinitions: result.credentialDefinitions.map((item) => item.credentialDefinition)
+    }
   }
 
   async findAll(): Promise<Issuer[]> {
-    return (await this.databaseService.getConnection())
-      .select()
-      .from(issuers);
+    const result = await (await this.databaseService.getConnection()).query.issuers.findMany({
+      with: {
+        credentialDefinitions: {
+          with: {
+            credentialDefinition: {
+              with: {
+                icon: true,
+                attributes: true,
+                representations: true,
+                revocation: true
+              }
+            }
+          }
+        },
+        logo: true
+      },
+    })
+
+    return result.map(issuer => ({
+      ...issuer,
+      credentialDefinitions: issuer.credentialDefinitions.map(item => item.credentialDefinition)
+    }))
   }
 }
 

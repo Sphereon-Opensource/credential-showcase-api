c8b226ead83f9570aa08afd5befeea371baf12a7
A.G.J. Cate <brummos@gmail.com>
2025-02-21 09:34:28 +0100

    chore: pr comments

--- apps/credential-showcase-api-server/src/database/schema/stepAction.ts
+++ apps/credential-showcase-api-server/src/database/schema/stepAction.ts
@@ -7,13 +7,13 @@ export const stepActions = pgTable('stepAction', {
     id: uuid('id').notNull().primaryKey().defaultRandom(),
     type: varchar({ length: 255 }).notNull(),
     title: varchar({ length: 255 }).notNull(),
-    text: varchar({ length: 255 }).notNull(),
-    stepId: uuid('step_id').references(() => steps.id,{ onDelete: 'cascade' }).notNull()
+    text: varchar().notNull(),
+    step: uuid().references(() => steps.id,{ onDelete: 'cascade' }).notNull()
 });
 
 export const stepActionRelations = relations(stepActions, ({ one }) => ({
-    credentialDefinition: one(steps, {
-        fields: [stepActions.stepId],
+    step: one(steps, {
+        fields: [stepActions.step],
         references: [steps.id],
     }),
 }));

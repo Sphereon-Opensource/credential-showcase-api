8ba572ff7bf0ac0050860dc31f31731c85c301ea
Bram ten Cate <59436492+BtencateSphereon@users.noreply.github.com>
2025-02-21 16:21:20 +0100

    Update apps/credential-showcase-api-server/src/database/schema/persona.ts
    Co-authored-by: Ronaldo Lima <ronal2do@gmail.com>

--- apps/credential-showcase-api-server/src/database/schema/persona.ts
+++ apps/credential-showcase-api-server/src/database/schema/persona.ts
@@ -5,7 +5,7 @@ export const personas = pgTable("persona", {
   id: uuid('id').notNull().primaryKey().defaultRandom(),
   name: varchar({ length: 255 }).notNull(),
   role: varchar({ length: 255 }).notNull(),
-  description: varchar({ length: 255 }),
+  description: varchar({ length: 255 }).notNull(),
   headshotImageId: uuid("headshot_image_id").references(() => assets.id),
   bodyImageId: uuid("body_image_id").references(() => assets.id),
 });

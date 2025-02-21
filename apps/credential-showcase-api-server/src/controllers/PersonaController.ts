f3c4604c0d9164305f6592781df443aecfb39c8a
Ronaldo Lima <ronal2do@gmail.com>
2025-02-21 16:27:32 +0100

    feat: Update PersonaController and OpenAPI spec for single persona response

--- apps/credential-showcase-api-server/src/controllers/PersonaController.ts
+++ apps/credential-showcase-api-server/src/controllers/PersonaController.ts
@@ -13,6 +13,8 @@ import { Service } from 'typedi'
 import {
     PersonasResponse,
     PersonasResponseFromJSONTyped,
+    PersonaResponse,
+    PersonaResponseFromJSONTyped,
     PersonaRequest
 } from 'credential-showcase-openapi'
 import PersonaService from '../services/PersonaService';
@@ -29,22 +31,22 @@ class PersonaController {
     }
 
     @Get('/:id')
-    public async getOne(@Param('id') id: string): Promise<PersonasResponse> {
+    public async get(@Param('id') id: string): Promise<PersonaResponse> {
         const result = await this.personaService.get(id);
-        return PersonasResponseFromJSONTyped({ persona: result }, true)
+        return PersonaResponseFromJSONTyped({ persona: result }, true)
     }
 
     @HttpCode(201)
     @Post('/')
-    public async post(@Body() personaRequest: PersonaRequest): Promise<PersonasResponse> {
+    public async post(@Body() personaRequest: PersonaRequest): Promise<PersonaResponse> {
         const result = await this.personaService.create(personaRequest);
-        return PersonasResponseFromJSONTyped({ persona: result }, true)
+        return PersonaResponseFromJSONTyped({ persona: result }, true)
     }
 
     @Put('/:id')
-    public async put(@Param('id') id: string, @Body() personaRequest: PersonaRequest): Promise<PersonasResponse> {
+    public async put(@Param('id') id: string, @Body() personaRequest: PersonaRequest): Promise<PersonaResponse> {
         const result = await this.personaService.update(id, personaRequest)
-        return PersonasResponseFromJSONTyped({ persona: result }, true)
+        return PersonaResponseFromJSONTyped({ persona: result }, true)
     }
 
     @OnUndefined(204)

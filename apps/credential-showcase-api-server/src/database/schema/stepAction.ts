42b761ef7bc70d47316688c624462e4354f9521a
A.G.J. Cate <brummos@gmail.com>
2025-02-26 11:54:34 +0100

    chore: aries oob action fixes

--- apps/credential-showcase-api-server/src/database/schema/stepAction.ts
+++ apps/credential-showcase-api-server/src/database/schema/stepAction.ts
@@ -1,25 +1,16 @@
 import { relations } from 'drizzle-orm';
 import { pgTable, uuid, text } from 'drizzle-orm/pg-core';
 import { steps } from './step';
-import { StepActionType } from '../../types';
 import { ariesProofRequests } from './ariesProofRequest';
+import { StepActionType } from '../../types';
 
 export const stepActions = pgTable('stepAction', {
     id: uuid('id').notNull().primaryKey().defaultRandom(),
-    actionType: text().notNull().$type<StepActionType>(),
+    actionType: text('action_type').notNull().$type<StepActionType>(),
     title: text().notNull(),
     text: text().notNull(),
-    step: uuid().references(() => steps.id, { onDelete: 'cascade' }).notNull()
-  },
-  // () => [
-  //   check(
-  //     'type_check',
-  //     sql`
-  //           type IN ('ARIES_OOB')
-  //       `,
-  //   ),
-  // ],
-)
+    step: uuid().references(() => steps.id, { onDelete: 'cascade' }).notNull(),
+})
 
 export const stepActionRelations = relations(stepActions, ({ one }) => ({
     step: one(steps, {
